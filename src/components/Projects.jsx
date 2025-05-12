f3f8455a2fb2b7056cf36d52d9ef6bd35deb89e1
Flame787 <mbrezovic77@gmail.com>
2025-05-12 16:20:38 +0200

    added some procejt descriptions

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,6 +23,13 @@ export default function Projects() {
         {/* <Grid size={{ xs: 12, sm: 6, md: 4 }}> */}
         <Grid size={{ xs: 12 }}>
           <Card>
+            <Typography
+              variant="h6"
+              gutterBottom
+              sx={{ textAlign: "center", mt: 2, mb: 3 }}
+            >
+              React Chat App with Scaledrone Websocket
+            </Typography>
             <CardMedia
               component="img"
               height="140"
@@ -30,12 +37,28 @@ export default function Projects() {
               alt="Project 1"
             />
             <CardContent>
-              <Typography variant="h6" gutterBottom>
-                React Chat App with Scaledrone Websocket
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ mt: 2, mb: 2 }}
+              >
                 A brief description of project 1. It includes features X, Y, and
                 Z.
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ mt: 2, mb: 2 }}
+              >
+                text...
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ mt: 2, mb: 2 }}
+              >
+                Technologies:{" "}
+                <strong>React, REST API, Scaledrone Websocket </strong>
               </Typography>
               <Button
                 variant="contained"
@@ -52,6 +75,13 @@ export default function Projects() {
         {/* Project 2 */}
         <Grid size={{ xs: 12 }}>
           <Card>
+            <Typography
+              variant="h6"
+              gutterBottom
+              sx={{ textAlign: "center", mt: 2, mb: 3 }}
+            >
+              Music Search App powered by Spotify API
+            </Typography>
             <CardMedia
               component="img"
               height="140"
@@ -59,12 +89,28 @@ export default function Projects() {
               alt="Project 2"
             />
             <CardContent>
-              <Typography variant="h6" gutterBottom>
-                Music Search App powered by Spotify API
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ mt: 2, mb: 2 }}
+              >
                 A brief description of project 2. It features unique
                 functionality.
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ mt: 2, mb: 2 }}
+              >
+                text...
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ mt: 2, mb: 2 }}
+              >
+                Technologies:{" "}
+                <strong>JavaScript, SCSS, REST API, Express.js </strong>
               </Typography>
               <Button
                 variant="contained"
@@ -81,6 +127,13 @@ export default function Projects() {
         {/* Project 3 */}
         <Grid size={{ xs: 12 }}>
           <Card>
+            <Typography
+              variant="h6"
+              gutterBottom
+              sx={{ textAlign: "center", mt: 2, mb: 3 }}
+            >
+              Tolkien info page using OpenLibrary API
+            </Typography>
             <CardMedia
               component="img"
               height="140"
@@ -88,12 +141,30 @@ export default function Projects() {
               alt="Project 3"
             />
             <CardContent>
-              <Typography variant="h6" gutterBottom>
-                Tolkien info page using OpenLibrary API
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ mt: 2, mb: 2 }}
+              >
+                A fan-made landing page dedicated to J.R.R. Tolkien’s legacy,
+                featuring biographical information, his published works,
+                illustrations and external resources.
               </Typography>
-              <Typography variant="body2" color="text.secondary">
-                A brief description of project 3. It is focused on aspect A, B,
-                and C.
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ mt: 2, mb: 2 }}
+              >
+                The application integrates with the Open Library API to fetch
+                and display books authored by Tolkien, allowing users to explore
+                and sort them by current ratings.
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ mt: 2, mb: 2 }}
+              >
+                Technologies: <strong>JavaScript, Bootstrap, REST API </strong>
               </Typography>
               <Button
                 variant="contained"
